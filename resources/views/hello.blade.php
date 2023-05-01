b9abe8d5e6281bce1b136ccc966c52d863938ea8
sakitayu <sakitakitakita@gmail.com>
2023-05-01 21:57:59 +0900

    ビューを設定

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang=ja>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <p>こにちゃ{{ $name }}さん</p>
+    <p>本コースでは{{ $course }}を学習します</p>
+</body>
+
+</html>
---
+++ resources/views/hello_world.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang=ja>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <p style="color: red">こにちゃ</p>
+</body>
+
+</html>
